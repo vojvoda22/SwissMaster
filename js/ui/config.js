b9d74b82c7d0021b7465cd945dbd51c29a3990d2
Soham Rath <135379592+soham-rath@users.noreply.github.com>
2026-02-16 15:30:48 +0100

    Add files via upload

--- js/ui/config.js
+++ js/ui/config.js
@@ -1,27 +1,27 @@
-const INITIAL_NAME = "Schachturnier";
-const INITIAL_TEAMS = ["Team 1", "Team 2", "Team 3", "Team 4"];
-const RULES_PRESETS = {
-  FIDE: {
-    pointsMatchWin: 2,
-    pointsMatchDraw: 1,
-    pointsMatchLoss: 0,
-    pointsBye: 2,
-  },
-  USCF: {
-    pointsMatchWin: 1,
-    pointsMatchDraw: 0.5,
-    pointsMatchLoss: 0,
-    pointsBye: 1,
-  },
-  CUSTOM: null,
-};
-const CONSTANTS = {
-  WIN: 1,
-  DRAW: 0.5,
-  LOSS: 0,
-  MATCH_WIN: 2,
-  MATCH_DRAW: 1,
-  MATCH_LOSS: 0,
-  BYE_MP: 2,
-  BYE_BP: 4,
-};
+  const INITIAL_NAME = "Schachturnier";
+  const INITIAL_TEAMS = ["Team 1", "Team 2", "Team 3", "Team 4"];
+  const RULES_PRESETS = {
+    FIDE: {
+      pointsMatchWin: 2,
+      pointsMatchDraw: 1,
+      pointsMatchLoss: 0,
+      pointsBye: 2,
+    },
+    USCF: {
+      pointsMatchWin: 1,
+      pointsMatchDraw: 0.5,
+      pointsMatchLoss: 0,
+      pointsBye: 1,
+    },
+    CUSTOM: null,
+  };
+  const CONSTANTS = {
+    WIN: 1,
+    DRAW: 0.5,
+    LOSS: 0,
+    MATCH_WIN: 2,
+    MATCH_DRAW: 1,
+    MATCH_LOSS: 0,
+    BYE_MP: 2,
+    BYE_BP: 4,
+  };
